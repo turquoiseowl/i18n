bbf0a2d44d959511dfa87e7063829df893781ade
Martin Connell <martinc@topology.com>
2013-04-15 12:23:31 +0100

    NuggetParser class added along with supporting classes: NuggetTokens and Nugget.
    Corresponding unit tests added.

---
+++ src/i18n.Domain.Tests/NuggetParserTests.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using i18n.Helpers;
+
+namespace i18n.Domain.Tests
+{
+    [TestClass]
+    public class NuggetParserTests
+    {
+        private void CompareNugget(string nuggetString, Nugget rhs, bool equal = true)
+        {
+           // Arrange.
+            NuggetTokens nuggetTokens = new NuggetTokens("[[[", "]]]", "|||", "///");
+            NuggetParser nuggetParser = new NuggetParser(nuggetTokens);
+           // Act.
+            Nugget nugget = nuggetParser.BreakdownNugget(nuggetString);
+           // Assert.
+            if (equal) {
+                Assert.AreEqual(nugget, rhs); }
+            else {
+                Assert.AreNotEqual(nugget, rhs); }
+        }
+
+        [TestMethod]
+        public void NuggetParser_CanBreakdownNugget()
+        {
+            CompareNugget("[[[msgid]]]", new Nugget { MsgId = "msgid" });
+            CompareNugget("[[[msgid|||{0}]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{0}" } });
+            CompareNugget("[[[msgid|||{0}|||{1}]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{0}", "{1}" } });
+            CompareNugget("[[[msgid|||{0}|||{1}]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{0}", "{0}" } }, false);
+            CompareNugget("[[[msgid|||{0}|||{1}]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{1}", "{0}" } }, false);
+            CompareNugget("[[[msgid|||{0}|||{1}|||{3}|||{4}|||{2}|||{5}]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{0}", "{1}", "{3}", "{4}", "{2}", "{5}" } });
+            CompareNugget("[[[msgid|||{0}///comment]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{0}" }, Comment = "comment" });
+            CompareNugget("[[[msgid|||{0}/// comment]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{0}" }, Comment = "comment" }, false);
+            CompareNugget("[[[msgid|||{0}/// comment]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{0}" }, Comment = " comment" });
+            CompareNugget("[[[msgid|||{0}|||{1}|||{3}|||{4}|||{2}|||{5}/// comment ]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "{0}", "{1}", "{3}", "{4}", "{2}", "{5}" }, Comment = " comment " });
+            CompareNugget("[[[msgid/// comment ]]]", new Nugget { MsgId = "msgid", Comment = " comment " });
+            CompareNugget("[[[msgid// comment ]]]", new Nugget { MsgId = "msgid", Comment = " comment " }, false);
+            CompareNugget("[[[msgid// comment ]]]", new Nugget { MsgId = "msgid// comment ", Comment = " comment " }, false);
+            CompareNugget("[[[msgid// comment ]]]", new Nugget { MsgId = "msgid// comment " });
+            CompareNugget("[[[msgid|| comment ]]]", new Nugget { MsgId = "msgid|| comment " });
+            CompareNugget("[[[msgid|||| comment ]]]", new Nugget { MsgId = "msgid", FormatItems = new string[] { "| comment " } });
+        }
+
+        [TestMethod]
+        public void NuggetParser_CanParseEntity01()
+        {
+           // Arrange.
+            NuggetTokens nuggetTokens = new NuggetTokens("[[[", "]]]", "|||", "///");
+            NuggetParser nuggetParser = new NuggetParser(nuggetTokens);
+            string entity = "<p>[[[hello]]]</p><p>[[[there]]]</p>";
+           // Act.
+            nuggetParser.ParseString(entity, delegate(string nuggetString, int pos, Nugget nugget1, string entity1)
+            {
+                if (pos == 3) {
+                    Assert.AreEqual(nugget1, new Nugget { MsgId = "hello" }); }
+                else if (pos == 21) {
+                    Assert.AreEqual(nugget1, new Nugget { MsgId = "there" }); }
+                else {
+                    Assert.Fail(); }
+                return null;
+            });
+        }
+
+        [TestMethod]
+        public void NuggetParser_CanParseEntity02()
+        {
+           // Arrange.
+            NuggetTokens nuggetTokens = new NuggetTokens("[[[", "]]]", "|||", "///");
+            NuggetParser nuggetParser = new NuggetParser(nuggetTokens);
+            string entity = "<p>[[[hello|||{0}]]]</p><p>[[[there]]]</p>";
+           // Act.
+            nuggetParser.ParseString(entity, delegate(string nuggetString, int pos, Nugget nugget1, string entity1)
+            {
+                if (pos == 3) {
+                    Assert.AreEqual(nugget1, new Nugget { MsgId = "hello", FormatItems = new string[] { "{0}" } }); }
+                else if (pos == 27) {
+                    Assert.AreEqual(nugget1, new Nugget { MsgId = "there" }); }
+                else {
+                    Assert.Fail(); }
+                return null;
+            });
+        }
+
+        [TestMethod]
+        public void NuggetParser_CanParseEntity03()
+        {
+           // Arrange.
+            NuggetTokens nuggetTokens = new NuggetTokens("[[[", "]]]", "|||", "///");
+            NuggetParser nuggetParser = new NuggetParser(nuggetTokens);
+            string entity = "<p>[[[hello|||{0}]]]</p><p>[[[there|||{0}|||{1}///comment comment comment]]]</p>";
+           // Act.
+            nuggetParser.ParseString(entity, delegate(string nuggetString, int pos, Nugget nugget1, string entity1)
+            {
+                if (pos == 3) {
+                    Assert.AreEqual(nugget1, new Nugget { MsgId = "hello", FormatItems = new string[] { "{0}" } }); }
+                else if (pos == 27) {
+                    Assert.AreEqual(nugget1, new Nugget { MsgId = "there", FormatItems = new string[] { "{0}", "{1}" }, Comment = "comment comment comment" }); }
+                else {
+                    Assert.Fail(); }
+                return null;
+            });
+        }
+
+        private void CanParseEntity_CustomNuggetTokens_Act(string entity, NuggetParser nuggetParser)
+        {
+           // Act.
+            int i = 0;
+            nuggetParser.ParseString(entity, delegate(string nuggetString, int pos, Nugget nugget1, string entity1)
+            {
+                switch (i++) {
+                    case 0: {
+                        Assert.AreEqual(nugget1, new Nugget { MsgId = "hello", FormatItems = new string[] { "{0}" } });
+                        break;
+                    }
+                    case 1: {
+                        Assert.AreEqual(nugget1, new Nugget { MsgId = "there", FormatItems = new string[] { "{0}", "{1}" }, Comment = "comment comment comment" });
+                        break;
+                    }
+                    default:
+                        Assert.Fail();
+                        break;
+                }
+                return null;
+            });
+        }
+
+        [TestMethod]
+        public void NuggetParser_CanParseEntity_CustomNuggetTokens01()
+        {
+           // Arrange.
+            NuggetTokens nuggetTokens = new NuggetTokens("[[[[", "]]]]]", "||", "//");
+            NuggetParser nuggetParser = new NuggetParser(nuggetTokens);
+            string entity = "<p>[[[[hello||{0}]]]]]</p><p>[[[[there||{0}||{1}//comment comment comment]]]]]</p>";
+           // Act.
+            CanParseEntity_CustomNuggetTokens_Act(entity, nuggetParser);
+        }
+
+        [TestMethod]
+        public void NuggetParser_CanParseEntity_CustomNuggetTokens02()
+        {
+           // Arrange.
+            NuggetTokens nuggetTokens = new NuggetTokens("[[[:", ":]]]", "|||", "///");
+            NuggetParser nuggetParser = new NuggetParser(nuggetTokens);
+            string entity = "<p>[[[:hello|||{0}:]]]</p><p>[[[:there|||{0}|||{1}///comment comment comment:]]]</p>";
+           // Act.
+            CanParseEntity_CustomNuggetTokens_Act(entity, nuggetParser);
+        }
+
+        [TestMethod]
+        public void NuggetParser_CanParseEntity_CustomNuggetTokens03()
+        {
+           // Arrange.
+            NuggetTokens nuggetTokens = new NuggetTokens("```", "'''", "###", "@@@");
+            NuggetParser nuggetParser = new NuggetParser(nuggetTokens);
+            string entity = "<p>```hello###{0}'''</p><p>```there###{0}###{1}@@@comment comment comment'''</p>";
+           // Act.
+            CanParseEntity_CustomNuggetTokens_Act(entity, nuggetParser);
+        }
+
+        [TestMethod]
+        public void NuggetParser_CanParseEntity_MultiLineNugget01()
+        {
+           // Arrange.
+            NuggetTokens nuggetTokens = new NuggetTokens("[[[", "]]]", "|||", "///");
+            NuggetParser nuggetParser = new NuggetParser(nuggetTokens);
+            string entity = "<p>[[[hello\r\n%0|||{0}]]]</p><p>[[[there]]]</p>";
+           // Act.
+            int i = 0;
+            nuggetParser.ParseString(entity, delegate(string nuggetString, int pos, Nugget nugget1, string entity1)
+            {
+                switch (i++) {
+                    case 0: {
+                        Assert.AreEqual(nugget1, new Nugget { MsgId = "hello\r\n%0", FormatItems = new string[] { "{0}" } });
+                        break;
+                    }
+                    case 1: {
+                        Assert.AreEqual(nugget1, new Nugget { MsgId = "there" });
+                        break;
+                    }
+                    default:
+                        Assert.Fail();
+                        break;
+                }
+                return null;
+            });
+        }
+    }
+}
---
+++ src/i18n.Domain/Helpers/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace i18n.Helpers
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// String extension method to simplify testing for non-null/non-empty values.
+        /// </summary>
+        public static bool IsSet(
+            this string str)
+        {
+            return !string.IsNullOrEmpty(str);
+        }
+
+        /// <summary>
+        /// Returns the line number (1-based) of the identified character in the string.
+        /// </summary>
+        public static int LineFromPos(this string S, int Pos)
+        {
+            int Res = 1;
+            for (int i = 0; i < Pos; i++) {
+                if (S[i] == '\n') {
+                    Res++; }
+            }
+            return Res;
+        }
+    }
+}
