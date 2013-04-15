61e090e9600601e92292b5636aac1dd9dbb8f4f4
Martin Connell <martinc@topology.com>
2013-04-15 17:19:04 +0100

    Tidying up.

--- src/i18n.PostBuild/Program.cs
+++ src/i18n.PostBuild/Program.cs
@@ -16,36 +16,11 @@ namespace i18n.PostBuild
 	        var items = nugget.ParseAll();
 	        rep.SaveTemplate(items);
 
-			Translation translation = rep.GetLanguage("sv");
+			Translation translation = rep.GetLanguage("fr");
 			TranslationSynchronization ts = new TranslationSynchronization(rep);
 	        ts.SynchronizeTranslation(items, translation);
-			//  
-			//rep.SaveTranslation(translation);	        
 
-            if(args.Length == 0)
-            {
-                Console.WriteLine("This post build task requires passing in the $(ProjectDirectory) path");
-                return;
-            }
-
-            var path = args[0];
-            path = path.Trim(new[] {'\"'});
-
-            string gettext = null;
-            string msgmerge = null;
-
-            for (int i = 1; i < args.Length; i++)
-            {
-                if (args[i].StartsWith("gettext:", StringComparison.InvariantCultureIgnoreCase))
-                    gettext = args[i].Substring(8);
-
-                if (args[i].StartsWith("msgmerge:", StringComparison.InvariantCultureIgnoreCase))
-                    msgmerge = args[i].Substring(9);
-            }
-
-            //new PostBuildTask().Execute(path, gettext, msgmerge);
-
-
+            Console.WriteLine("i18n.PostBuild completed successfully.");
         }
     }
 }
