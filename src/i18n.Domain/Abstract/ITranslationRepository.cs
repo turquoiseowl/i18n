f5a3f9caa05b12b42702ed1ec9de4b8b543e0eec
Rickard Liljeberg <rickard@liljeberg.se>
2013-04-16 12:25:51 +0200

    cleanup

--- src/i18n.Domain/Abstract/ITranslationRepository.cs
+++ src/i18n.Domain/Abstract/ITranslationRepository.cs
@@ -13,14 +13,10 @@ namespace i18n.Domain.Abstract
 	/// </summary>
 	public interface ITranslationRepository
 	{
-		//IQueryable<TranslateItem> GetLanguageItems(string tag); //MC001 TODO: method redundant now that Translation.Items is a dictionary.
 		Translation GetLanguage(string tag);
-		//ConcurrentDictionary<string, TranslateItem> GetLanguageDictionary(string tag); //MC001 TODO: method redundant now that Translation.Items is a dictionary.
 		IEnumerable<Language> GetAvailableLanguages();
 		bool TranslationExists(string tag);
 		void SaveTranslation(Translation translation);
 		void SaveTemplate(IDictionary<string, TemplateItem> items);
-
-		//todo: consider adding function for deleting cache so that if cache is implemented in a concrete it will know when to reload.
 	}
 }
--- src/i18n/TextLocalizer.cs
+++ src/i18n/TextLocalizer.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Web;
 using System.Web.Caching;
 using System.Web.Hosting;
+using i18n.Domain.Entities;
 
 namespace i18n
 {
