43e401954df6b60370b38a813f8f4e51fb45da72
Martin Connell <martinc@topology.com>
2013-04-20 17:48:52 +0100

    Documented DirectoriesToScan

--- src/i18n.Domain/Concrete/i18nSettings.cs
+++ src/i18n.Domain/Concrete/i18nSettings.cs
@@ -267,6 +267,20 @@ namespace i18n.Domain.Concrete
 		#region DirectoriesToScan
 
 		private const string _directoriesToScan = ".";
+
+        /// <summary>
+        /// Specifies one or more paths to the root directory/folder of the branches
+        /// which FileNuggetParser is to scan for source files.
+        /// </summary>
+        /// <remarks>
+        /// Each string may be an absolute (rooted) path, or a path
+        /// relative to the folder containing the current config file
+        /// (<see cref="AbstractSettingService.GetConfigFileLocation"/>).<br/>
+        /// Default value is "." which equates to the the single folder containing the 
+        /// current config file (<see cref="AbstractSettingService.GetConfigFileLocation"/>).<br/>
+        /// Typically, you may set to ".." equating to the solution folder for the
+        /// project containing the current config file.
+        /// </remarks>
 		public virtual IEnumerable<string> DirectoriesToScan
 		{
 			get
