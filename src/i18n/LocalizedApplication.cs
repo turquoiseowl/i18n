a7b25f64e9c85619c4b09c8511dae9668f6911c6
couraud <roly.walter@couraud.com>
2014-06-26 12:41:10 +0100

    Merging

--- src/i18n/LocalizedApplication.cs
+++ src/i18n/LocalizedApplication.cs
@@ -67,7 +67,6 @@ namespace i18n
         ///     "zh-123"        [language + region]
         ///     "zh-Hant"       [language + script]
         ///     "zh-Hant-HK"    [language + script + region]
-        ///     "zh-Hant-HK-x-ABCD"    [language + script + region + private use]
         /// </remarks>
         public string DefaultLanguage { 
             get {
@@ -138,11 +137,11 @@ namespace i18n
         public bool PermanentRedirects { get; set; }
 
         /// <summary>
-        /// Regular expression that controls the ContextTypes elligible for Late URL Localization.
+        /// Regular expression that controls the ContextTypes elligible for response localization.
         /// </summary>
         /// <remarks>
         /// Set to null to disable Late URL Localization.<br/>
-        /// Defaults to @"^(?:(?:(?:text|application)/(?:plain|html|xml|javascript|json))(?:\s*;.*)?)$").<br/>
+        /// Defaults to @"^(?:(?:(?:text|application)/(?:plain|html|xml|javascript|x-javascript|json|x-json))(?:\s*;.*)?)$.<br/>
         /// Client may customise this member, for instance in Application_Start.<br/>
         /// This feature requires the LocalizedModule HTTP module to be intalled in web.config.<br/>
         /// Explanation of the default regex:<br/>
@@ -158,11 +157,11 @@ namespace i18n
         /// Regular expression that excludes certain URL paths from being localized.
         /// </summary>
         /// <remarks>
-        /// Defaults to ...<br/>
+        /// Defaults to excluding all less and css files and any URLs containing the phrases i18nSkip, glimpse, trace or elmah (case-insensitive)<br/>
         /// Clients may customise this member in Application_Start<br/>
         /// This feature requires the LocalizedModule HTTP module to be intalled in web.config.<br/>
         /// </remarks>
-        public Regex UrlsToExcludeFromProcessing = new Regex(@"(?:\.(?:axd|less|css)(?:\?|$))|(?i:i18nSkip|glimpse|trace|elmah)");
+        public Regex UrlsToExcludeFromProcessing = new Regex(@"(?:\.(?:less|css)(?:\?|$))|(?i:i18nSkip|glimpse|trace|elmah)");
 
 
         public LocalizedApplication()
