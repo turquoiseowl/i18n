69c6161ac0f3e2494f7862b766b96a5dc7c719ee
Sean Nolan <snolan@emmis.com>
2015-04-06 12:42:16 -0500

    Added Thread ID to debugging output to make it easier to differentiate between output from multiple requests.

--- src/i18n/Helpers/DebugHelpers.cs
+++ src/i18n/Helpers/DebugHelpers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 
 namespace i18n.Helpers
 {
@@ -9,18 +10,20 @@ namespace i18n.Helpers
         [Conditional("DEBUG")]
         public static void WriteLine(string message)
         {
-            string str = String.Format("+++> {0} -- {1}",
+            string str = String.Format("+++> {0}, Thread ID: {1} -- {2}",
                 DateTime.Now.ToString(),
+                Thread.CurrentThread.ManagedThreadId,
                 message);
             Debug.WriteLine(str);
         }
         [Conditional("DEBUG")]
         public static void WriteLine(string format, params object[] args)
         {
-            string str = String.Format("+++> {0} -- {1}",
+            string str = String.Format("+++> {0}, Thread ID: {1} -- {2}",
                 DateTime.Now.ToString(),
+                Thread.CurrentThread.ManagedThreadId,
                 format);
-            Debug.WriteLine(str, args);
+            Debug.WriteLine(string.Format(str, args));
         }
     }
 }
