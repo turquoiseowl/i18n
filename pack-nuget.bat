c092ff47ae27fc017e6548cfbbf4a1d3a6aa6408
Martin Connell <martinc@topology.com>
2015-05-27 18:05:07 +0100

    prep merging v2.0 into master

--- pack-nuget.bat
+++
@@ -1,8 +0,0 @@
-if not exist "bin" mkdir "bin"
-if not exist "bin\content" mkdir "bin\content"
-if not exist "bin\tools" mkdir "bin\tools"
-if not exist "bin\tools\gettext-0.14.4" mkdir "bin\tools\gettext-0.14.4"
-copy LICENSE.md bin
-copy README.md bin
-copy "tools\gettext-0.14.4\*.*" "bin\tools\gettext-0.14.4"
-".nuget\NuGet.exe" pack i18n.nuspec -BasePath bin
