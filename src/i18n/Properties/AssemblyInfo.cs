350c39c2dfd5714a6b22a656ece21b12dac399c7
Martin Connell <martinc@topology.com>
2015-05-27 18:23:15 +0100

    Increment version following NuGet publish

--- src/i18n/Properties/AssemblyInfo.cs
+++ src/i18n/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.1.1.0")] // picked up by nuspec $version$ identifier
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")] // picked up by nuspec $version$ identifier
+[assembly: AssemblyFileVersion("2.1.2.0")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
