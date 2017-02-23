241e351f6ddeb56938358eb9b656912f6fb83e40
Martin Connell <martinc@topology.com>
2017-02-23 18:39:46 +0000

    Version number bumped to 2.1.11-pre002

--- src/i18n.POTGenerator/Properties/AssemblyInfo.cs
+++ src/i18n.POTGenerator/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.1.11")]
 [assembly: AssemblyFileVersion("2.1.11")]
-[assembly: AssemblyInformationalVersion("2.1.11-pre001")]
+[assembly: AssemblyInformationalVersion("2.1.11-pre002")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
